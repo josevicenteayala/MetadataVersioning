7167768a2c1784f8febb400a2d223dcd7abd239b
Jose Vicente Ayala Luna <7784390+josevicenteayala@users.noreply.github.com>
2025-11-24 10:31:31 -0500

    Handle missing Docker by skipping Testcontainers tests

--- src/test/java/com/metadata/versioning/e2e/MetadataVersioningE2ETest.java
+++ src/test/java/com/metadata/versioning/e2e/MetadataVersioningE2ETest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
-@Testcontainers
+@Testcontainers(disabledWithoutDocker = true)
 class MetadataVersioningE2ETest {
 
     @Container

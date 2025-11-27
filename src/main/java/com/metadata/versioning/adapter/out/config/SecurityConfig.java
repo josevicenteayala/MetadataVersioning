47b78d7526ea5bf706e861d582777a3f95d64463
Jose Vicente Ayala Luna <7784390+josevicenteayala@users.noreply.github.com>
2025-11-27 02:43:28 -0500

    Fix missing Profile import in SecurityConfig

--- src/main/java/com/metadata/versioning/adapter/out/config/SecurityConfig.java
+++ src/main/java/com/metadata/versioning/adapter/out/config/SecurityConfig.java
@@ -1,8 +1,9 @@
 package com.metadata.versioning.adapter.out.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
